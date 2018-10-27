2a3fa471bcdf80482aca59585d5471ce4059338f
Jennael <39685710+Jennael@users.noreply.github.com>
2018-10-27 18:45:29 +0800

    Completed PSet1 Pt1 Qn 7, Qn 8
    Qn 7 Vocareum failed test cases 7, 9 and 17

---
+++ Robot/GetPath.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class GetPath {
+    /*
+    The arguments r, c specify the destination spot. 
+    The method returns true and the path, if such a path can be found, or false if such a path cannot be found.
+    Path if an ArrayList of the form [(0,0), (0,1), (1,1), (2,1), (2,2), (3,2)]. (row, column)
+    Grid specify which spots are blocked and which aren't. 
+    */
+    public static boolean getPath (int r, int c, ArrayList<Point> path, final int [][] grid) {
+
+        Point destination = new Point (r,c);
+        Point current = new Point (0,0);
+
+        return tryPath(current, destination, path, grid);
+
+    }
+
+    private static boolean tryPath (Point current, Point destination, ArrayList<Point> path, final int [][] grid){
+        path.add(current);
+
+        //check reached destination
+        if (current.x == destination.x && current.y == destination.y){
+            return true;
+        } 
+        //check overshot
+        else if (current.x > destination.x || current.y > destination.y){
+            path.remove(current);
+            return false;
+        }
+        //check blocked
+        else if (grid[current.x][current.y]==1){
+            path.remove(current);
+            return false;
+        }
+
+        Point right = new Point(current.x, current.y+1);
+        Point down = new Point(current.x+1, current.y);
+
+        if (tryPath(right, destination, path, grid)){
+            return true;
+        } else if (tryPath(down, destination, path, grid)){
+            return true;
+        } else {        //check no path
+            path.remove(current);
+            return false;
+        }
+
+    }
+
+}
+
+
+
+
+
+
