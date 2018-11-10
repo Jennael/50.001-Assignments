b4ebe4d585b774173081f96f93d2d8f792133a98
Jennael <39685710+Jennael@users.noreply.github.com>
2018-11-10 17:41:57 +0800

    Update Exception/TestException.java

--- Exception/TestException.java
+++ Exception/TestException.java
@@ -9,8 +9,14 @@ public class TestException {
         ret = tstException(-1, in);
 
         System.out.println(ret);
-    }
 
+        try {
+            int a = quotientInt(5, 0);
+        } catch (ArithmeticException ex){ //Division by 0 as denominator
+            ex.printStackTrace();
+        }
+
+    }
 
     public static String tstException(int idx, String[] y) {
         // implement using try-catch 
@@ -22,4 +28,15 @@ public class TestException {
         }
 
     }
+
+    public static int quotientInt (int a, int b){
+        return a/b;
+    }
+
+    public static int quotientDoublet (double a, double b) 
+    throws ArithmeticException{
+        if (b==0) throw new ArithmeticException("b is zero");
+        return a/b;
+    }
+
 }
